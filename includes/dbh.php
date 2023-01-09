2b16943e11abc276e15aee2dad94af568427b68b
DNAmaster10 <louishollobon@outlook.com>
2023-01-09 16:51:59 +0000

    Bug fix

--- includes/dbh.php
+++ includes/dbh.php
@@ -3,7 +3,7 @@
   $dbServername = "localhost";
   $dbUsername = getenv("DB_USERNAME");
   $dbPassword = getenv("DB_PASSWORD");
-  $dbName = ("tendors");
+  $dbName = ("tendor");
   $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
   if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQLi: " . mysqli_connect_error();

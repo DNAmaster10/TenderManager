5107dfd9a0a32e4f33e28eb4f5fe9ee495edaa78
DNAmaster10 <louishollobon@outlook.com>
2023-01-10 19:51:45 +0000

    update

--- pages/add.php
+++ pages/add.php
@@ -17,11 +17,11 @@
             <input type="text" placeholder="Client" id="client_input">
             <input type="text" placeholder="Year" id="year_input">
             <textarea placeholder="Answer" id="answer_input" rows="5" cols="50"></textarea>
+            <textarea placeholder="Additional Notes" id="additional_notes_input" rows="5" cols="50"></textarea>
             <input type="text" placeholder="Tag" id="tag_input" onkeyup="getTags()">
             <div id="tag_button_container"></div>
             <div id="added_tag_button_container"></div>
             <input type="hidden" id="tag_list">
-            <textarea placeholder="Additional Notes" id="additional_notes_input" rows="5" cols="50"></textarea>
             <button type="button" onclick="uploadInfo()">Submit</button>
             <p id="error"></p>
         </div>
--- pages/js/tags.js
+++ pages/js/tags.js
@@ -63,4 +63,10 @@ function addTag() {
         });
     }
 }
+function rename() {
+    
+}
+function openTagModal() {
+    
+}
 getTags();

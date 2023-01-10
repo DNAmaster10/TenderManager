5f0df8ef44b4f5006d6068a1aabcb2410a60be0a
DNAmaster10 <louishollobon@outlook.com>
2023-01-10 21:47:28 +0000

    test

--- pages/js/add.js
+++ pages/js/add.js
@@ -72,7 +72,15 @@ function getTags() {
         type: "GET",
         data: {tag:tagInput},
         success: function (data) {
-            addTagsToList(data);
+            document.getElementById("tag_button_container").innerHTML = "";
+            var tagArray = data.split("#-#");
+            for (var i = 0; i < tagArray.length; i++) {
+                if (!(tagArray[i].length < 1)) {
+                var element = "<button type='button' class='tag_select_button' value='" + tagArray[i] + "' onclick='addTag(this.value)'>" + tagArray[i] + "</button>";
+                var rootElement = document.getElementById("tag_button_container");
+                rootElement += element;
+        }
+    }
         }
     });
 }

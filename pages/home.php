264676f1ef0c68b326ea67a363739db4734d851c
DNAmaster10 <louishollobon@outlook.com>
2023-02-06 19:25:39 +0000

    update

--- pages/home.php
+++ pages/home.php
@@ -17,25 +17,27 @@
         </div>
         <div id="main_container">
             <h1>Welcome back, <?php echo $_SESSION["username"]; ?>.</h1>
-            <div id="search_form_container" class="action_form_container" onclick="document.forms['search'].submit();">
-                <form id="search" action="/pages/search.php" class="action_form">
-                    <input type="submit" value="Search" class="action_submit">
-                </form>
-            </div>
-            <div id="add_form_container" class="action_form_container" onclick="document.forms['add'].submit();">
-                <form id="add" action="/pages/add.php" class="action_form">
-                    <input type="submit" value="Add Entry" class="action_submit">
-                </form>
-            </div>
-            <div id="tags_form_container" class="action_form_container" onclick="document.forms['add'].submit();">
-                <form id="tags" action="/pages/tags.php" class="action_form">
-                    <input type="submit" value="Tags" class="action_submit">
-                </form>
-            </div>
-            <div id="list_form_container" class="action_form_container" onclick="document.forms['list'].submit();">
-                <form id="list" action="/pages/list.php" class="list_form">
-                    <input type="submit" value="Index" class="action_submit">
-                </form>
+            <div id="action_button_container">
+                <div id="search_form_container" class="action_form_container" onclick="document.forms['search'].submit();">
+                    <form id="search" action="/pages/search.php" class="action_form">
+                        <input type="submit" value="Search" class="action_submit">
+                    </form>
+                </div>
+                <div id="add_form_container" class="action_form_container" onclick="document.forms['add'].submit();">
+                    <form id="add" action="/pages/add.php" class="action_form">
+                        <input type="submit" value="Add Entry" class="action_submit">
+                    </form>
+                </div>
+                <div id="tags_form_container" class="action_form_container" onclick="document.forms['add'].submit();">
+                    <form id="tags" action="/pages/tags.php" class="action_form">
+                        <input type="submit" value="Tags" class="action_submit">
+                    </form>
+                </div>
+                <div id="list_form_container" class="action_form_container" onclick="document.forms['list'].submit();">
+                    <form id="list" action="/pages/list.php" class="list_form">
+                        <input type="submit" value="Index" class="action_submit">
+                    </form>
+                </div>
             </div>
         </div>
     </body>

8943963be2bd70f7cdd612099b12bb458b1e5557
DNAmaster10 <louishollobon@outlook.com>
2023-02-06 19:37:43 +0000

    update

--- pages/home.php
+++ pages/home.php
@@ -20,22 +20,22 @@
             <div id="action_button_container">
                 <div id="search_form_container" class="action_form_container" onclick="document.forms['search'].submit();">
                     <form id="search" action="/pages/search.php" class="action_form">
-                        <input type="submit" value="Search" class="action_submit">
+                        <p class="action_p">Search Database</p>
                     </form>
                 </div>
                 <div id="add_form_container" class="action_form_container" onclick="document.forms['add'].submit();">
                     <form id="add" action="/pages/add.php" class="action_form">
-                        <input type="submit" value="Add Entry" class="action_submit">
+                        <p class="action_p">Add Entry</p>
                     </form>
                 </div>
                 <div id="tags_form_container" class="action_form_container" onclick="document.forms['add'].submit();">
                     <form id="tags" action="/pages/tags.php" class="action_form">
-                        <input type="submit" value="Tags" class="action_submit">
+                        <p class="action_p">Tags</p>>
                     </form>
                 </div>
                 <div id="list_form_container" class="action_form_container" onclick="document.forms['list'].submit();">
                     <form id="list" action="/pages/list.php" class="list_form">
-                        <input type="submit" value="Index" class="action_submit">
+                        <p class="action_p">List Entries</p>
                     </form>
                 </div>
             </div>

adca169571a0f2717251245c3d1a3120d8423fbf
Louis <44494235+DNAmaster10@users.noreply.github.com>
2023-09-15 17:31:19 +0100

    test

--- pages/home.php
+++ pages/home.php
@@ -13,7 +13,11 @@
     <body>
         <div id="main_container">
 			<div id="sidebar">
-				
+				<button type="button" id="home_button" onclick="goHome()">Home</button>
+				<button type="button" id="search_button" onclick="goSearch()">Search</button>
+				<button type="button" id="add_entry_button" onclick="goEntry()">Add Entry</button>
+				<button type="button" id="tags_button" onclick="goTags()">Tags</button>
+				<button type="button" id="index_button" onclick="goIndex()">Index</button>
 			</div>
 			<div id="page_container">
 				<iframe src="/pages/welcome.php"></iframe>
---
+++ pages/js/home.js
@@ -0,0 +1,15 @@
+function goHome() {
+	document.getElementById("main_frame").src = "/pages/welcome.php";
+}
+function goSearch() {
+	document.getElementById("main_frame").src = "/pages/search.php";
+}
+function goEntry() {
+	document.getElementById("main_frame").src = "/pages/add.php";
+}
+function goTags() {
+	document.getElementById("main_frame").src = "/pages/tags.php";
+}
+function goIndex() {
+	document.getElementById("main_frame").src = "/pages/list.php";
+}
